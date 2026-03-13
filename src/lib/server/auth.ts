add1b9f3e04df4ac67f913e04a63c4ff8ed8880c
Stanislav Zeman <zeman@standa.dev>
2026-03-13 18:05:53 +0100

    fix: dynamically read env variables at runtime

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,13 +1,13 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 import { upsertUser } from "./queries/users.js";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [
     Google({
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
